75966831cc27df12cea6da5a596722e8fe5c35c9
pli60 <pli60@ucsc.edu>
2022-07-14 02:26:38 -0700

    Add backgrounds and random background

--- src/main.js
+++ src/main.js
@@ -2,15 +2,21 @@
 function preload(){
   
   tile_water = loadImage('assets/Water.png');
+  bg1 = loadImage('assets/Background1.png');
+  bg2 = loadImage('assets/Background2.png');
+  bg3 = loadImage('assets/Background3.png');
+  bg4 = loadImage('assets/Background4.png');
+  bg5 = loadImage('assets/Background5.png');
   
-
 }
 
 
 function setup() {
 
   createCanvas(canvas_width, canvas_height);
-  background(0);
+  let backg = [bg1, bg2, bg3, bg4, bg5];
+  bg = random(backg);
+  background(bg);
 
   //console.log("width = " + width + "  height = " + height);
 
