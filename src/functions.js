7c079123793b0290ffd7655e9cdf2f33232a109f
lxu58 <103284069+lxu58@users.noreply.github.com>
2022-07-14 11:30:43 -0700

    update

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,90 @@
+//=============================================================
+//========================Background===========================
+
+// function that preload the background
+function Background_preload() {
+    bg1 = loadImage('assets/Background1.png');
+    bg2 = loadImage('assets/Background2.png');
+    bg3 = loadImage('assets/Background3.png');
+    bg4 = loadImage('assets/Background4.png');
+    bg5 = loadImage('assets/Background5.png');
+}
+
+
+function Background_setup() {
+    let backg = [bg1, bg2, bg3, bg4, bg5];
+    bg = random(backg);
+    background(bg);
+}
+
+//====================Background End===========================
+//=============================================================
+
+
+
+
+//=============================================================
+//===========================Tile==============================
+
+// function preload every tile
+function Tile_preload() {
+    tile_water = loadImage('./assets/Water.png');
+    tile_grass = loadImage('./assets/Grass.png');
+    tile_road = loadImage('./assets/Road.png');
+}
+
+
+// function that make tilesmap
+function Tile_generator() {
+    for (var i = 0; i < rooms.length; i++) {
+
+        //draw assets based on the rows and columns
+        for (var row = 0; row < rooms[i].tiles_row; row++) {
+            for (var column = 0; column < rooms[i].tiles_column; column++) {
+                //seeds the tile generator
+                noiseSeed(seeds_noise);
+
+                //must push and pop before use translate 
+                push();
+                //translate the origin to start_x, start_y  (treat that point as 0, 0)
+                translate(rooms[i].start_x, rooms[i].start_y);
+
+                if (noise(row, column) > 0.5) {
+                    image(tile_water, row * tilesize, column * tilesize);
+                    rooms[i].tiles.push(new array_addTile(row, column, tileID_Water, tileID_None, tileID_None));
+                } else {
+                    image(tile_grass, row * tilesize, column * tilesize);
+                    rooms[i].tiles.push(new array_addTile(row, column, tileID_Grass, tileID_None, tileID_None));
+                }
+                pop();
+                seeds_noise += seeds_noise_increment;
+            }
+        }//for end
+
+    }
+
+
+    Tile_fixer();
+}
+
+
+// function that contains rule that fix the tile against the logic, 
+//          in other words : procedural generation 
+function Tile_fixer() {
+
+
+}
+
+//=========================Tile End============================
+//=============================================================
+
+
+
+
 
 //  print out data for debugging
 function printRoomData() {
     for (var i = 0; i < rooms.length; i++) {
-
         console.log("ID : " + rooms[i].room_ID);
         console.log("start_x : " + rooms[i].start_x);
         console.log("start_y : " + rooms[i].start_y);
@@ -10,7 +92,20 @@ function printRoomData() {
         console.log("room_height : " + rooms[i].room_height);
         console.log("");
     }
-
 }
+
+//  print out data for debugging
+
+function printRoomTileData() {
+    for (var i = 0; i < rooms.length; i++) {
+        console.log("ID : " + rooms[i].room_ID);
+        
+        for(var j = 0; j < rooms[i].tiles.length ; j ++){
+            console.log("row: " + rooms[i].tiles[j].tile_row + " column: " + rooms[i].tiles[j].tile_column);
+            console.log( rooms[i].tiles[j].layer_0 +  " "  +  rooms[i].tiles[j].layer_1 + " " + rooms[i].tiles[j].layer_2)
+        }
+    }
+}
+
 
 
--- src/init.js
+++ src/init.js
@@ -4,8 +4,13 @@
 var canvas_width = 1280;            // canvas w and h
 var canvas_height = 720;
 
-var minSize = canvas_width / 2;     // used to determine the min size of each room 
+var minSize = canvas_width / 1;     // used to determine the min size of each room 
 var maxSplitLevel = 10;             // height of the BSP tree, determine the variety of the rooms
+
+//seeds setup
+var seeds = 99;                     // change this once finish the program, right now make it constant for debugging
+var seeds_noise = 0;
+var seeds_noise_increment = 0.01;
 
 //road setup 
 var road_color = [150, 150, 150];    // road rgb
@@ -13,12 +18,22 @@ var road_stroke = 50;                // road stroke
 
 //room setup
 var room_color = [150, 150, 150];    // road rgb
+var room_ID = 0;                     // init room id to 000
 
 //borders
 var border_color = [255, 255, 255];  // road rgb
 
+
 //rooms control
 var rooms = new Array();             // create array
+
+//tiles
+var tilesize = 32;                    //tile size should be 32  *  32
+
+var tileID_None = 00;
+var tileID_Grass = 01;
+var tileID_Water = 02;
+var tileID_Road = 03;
 
 //----------------------Global End-----------------------------
 //-------------------------------------------------------------
@@ -27,6 +42,19 @@ var rooms = new Array();             // create array
 
 //----------------------Objects--------------------------------
 //-------------------------------------------------------------
+
+//object that contains the information of each tile
+/*    tile_row, tile_column are the position of the tiles
+      layer is the complexity of the tile, pass tileID (can be found in init.js->global->tiles)
+      layer 0 is default tile type 
+*/
+function array_addTile(tile_row, tile_column, layer_0, layer_1, layer_2) {
+  this.tile_row = tile_row;
+  this.tile_column = tile_column;
+  this.layer_0 = layer_0;
+  this.layer_1 = layer_1;
+  this.layer_2 = layer_2;
+}
 
 //object that contains the information of each room, for quick draw
 /*    room_ID is reserved but not used, set to 0 by default
@@ -39,6 +67,15 @@ function array_addRoom(room_ID, start_x, start_y, room_width, room_height) {
   this.start_y = start_y;
   this.room_width = room_width;
   this.room_height = room_height;
+  this.tiles_row = room_width / tilesize;
+  this.tiles_column = room_height / tilesize;
+
+  this.tiles = new Array();
 }
+
+
+
+
+
 //----------------------Objects End----------------------------
 //-------------------------------------------------------------
