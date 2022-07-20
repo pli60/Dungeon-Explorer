47a7ddfe4ba7eccc9e44c06772fb0612129e86d0
lxu58 <103284069+lxu58@users.noreply.github.com>
2022-07-19 20:11:19 -0700

    1.1

--- src/main.js
+++ src/main.js
@@ -15,7 +15,8 @@ function setup() {
   createCanvas(canvas_width, canvas_height);
 
 
-  var splitLevel = random (1,4);
+
+  var splitLevel = random(1, 4);
   var minSize = canvas_width / splitLevel;      // used to determine the min size of each room 
   var dungeon = generateDungeon(maxSplitLevel, minSize); // start dungeon generation
   dungeon.generateRoom(); // generates and draws the rooms
@@ -30,18 +31,7 @@ function setup() {
 
 function draw() {
 
-
-
   draw_map();
   draw_text();
-  reset_init();
 
-
-}
-
-
-function reset_init(){
-  // room_canvas = [];
-  // rooms = [];
-  //seeds_noise = 0;
 }
